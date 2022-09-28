354d856725fd85019da75fba0ce37ae76d164e9c
MichielDeCapThomasMore <r0879855@student.thomasmore.be>
2022-09-28 12:04:40 +0200

    Working version of buttons using custom colors

--- dinowebsite/src/components/button.tsx
+++ dinowebsite/src/components/button.tsx
@@ -1,11 +1,46 @@
 import type { NextPage } from "next";
 
-// variable with type NextPage with a color input
-const Button: NextPage<{ color: string }> = ({ color }) => {
-    console.log(color);
+// Dinoblack
+// Dinowhite
+// Dinocream
+// Dinogreen
+// Dinobiege
+// Dinobrown
+// component with a button that has a dinoblack color 
+const ButtonDinoblack: NextPage = () => {
     return (
-        <button className={`bg-${color} hover:bg-${color} text-${color} font-bold py-2 px-4 rounded`}>eee</button>
-    );
-};
+        <button className="bg-dinoblack text-dinowhite font-bold py-2 px-4 rounded"></button>
+    )
+}
 
-export default Button;
+const ButtonDinowhite: NextPage = () => {
+    return (
+        <button className="bg-dinowhite text-dinoblack font-bold py-2 px-4 rounded"></button>
+    )
+}
+
+const ButtonDinocream: NextPage = () => {
+    return (
+        <button className="bg-dinocream text-dinoblack font-bold py-2 px-4 rounded"></button>
+    )
+}
+
+const ButtonDinogreen: NextPage = () => {
+    return (
+        <button className="bg-dinogreen text-dinoblack font-bold py-2 px-4 rounded"></button>
+    )
+}
+
+const ButtonDinobiege: NextPage = () => {
+    return (
+        <button className="bg-dinobiege text-dinoblack font-bold py-2 px-4 rounded"></button>
+    )
+}
+
+const ButtonDinobrown: NextPage = () => {
+    return (
+        <button className="bg-dinobrown text-dinowhite font-bold py-2 px-4 rounded"></button>
+    )
+}
+
+export { ButtonDinoblack, ButtonDinowhite, ButtonDinocream, ButtonDinogreen, ButtonDinobiege, ButtonDinobrown }
